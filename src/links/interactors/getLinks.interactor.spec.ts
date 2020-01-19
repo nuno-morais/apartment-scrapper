640c102c0baf1a2030d03718c57093083169f5f9
Nuno Morais <nram@nuno-morais.eu>
2020-01-19 17:07:42 +0000

    Improve build for heroku

--- src/links/interactors/getLinks.interactor.spec.ts
+++ src/links/interactors/getLinks.interactor.spec.ts
@@ -2,6 +2,6 @@ import { GetLinksInteractor } from './getLinks.interactor';
 
 describe('GetLinksInteractor', () => {
   it('should be defined', () => {
-    //expect(new GetLinksInteractor()).toBeDefined();
+    // expect(new GetLinksInteractor()).toBeDefined();
   });
 });
