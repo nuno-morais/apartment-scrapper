e6c10ce0f9cf03b577cfa9c726d72f4a156788f7
Nuno Morais <nram@nuno-morais.eu>
2020-01-19 18:10:58 +0000

    Remove logs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import * as audit from 'express-requests-logger';
-import * as bunyan from 'bunyan';
-import * as pretty from '@mechanicalhuman/bunyan-pretty';
 import * as cors from 'cors';
-import { ResponseHeadersInterceptor } from './common/interceptors/ResponseHeadersInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,28 +8,30 @@ async function bootstrap() {
   app.use(cors({
     exposedHeaders: ['X-Total-Count'],
   }));
-  const logger = bunyan.createLogger({
-    name: 'app',
-    stream: pretty(process.stdout, { timeStamps: false }),
-    level: 'info',
-  });
-  app.use(audit({
-    logger,
-    request: {
-      excludeHeaders: ['authorization'],
-      maxBodyLength: 200,
-    },
-    response: {
-      maskBody: ['session_token'],
-      maxBodyLength: 200,
-    },
-  }));
-  app.useGlobalInterceptors(new ResponseHeadersInterceptor());
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true,
-  }));
+  // const logger = bunyan.createLogger({
+  //   name: 'app',
+  //   stream: pretty(process.stdout, { timeStamps: false }),
+  //   level: 'info',
+  // });
+  // app.use(audit({
+  //   logger,
+  //   request: {
+  //     excludeHeaders: ['authorization'],
+  //     maxBodyLength: 200,
+  //   },
+  //   response: {
+  //     maskBody: ['session_token'],
+  //     maxBodyLength: 200,
+  //   },
+  // }));
+  // app.useGlobalInterceptors(new ResponseHeadersInterceptor());
+  // app.useGlobalPipes(new ValidationPipe({
+  //   transform: true,
+  // }));
   const defaultPort = 3001;
   const port: any = process.env.PORT || defaultPort;
+  // tslint:disable-next-line: no-console
+  console.log(`Starting server on :${port}`);
   await app.listen(port, () => {
     // tslint:disable-next-line
     console.log(`Server is listening on port ${port}.`);
